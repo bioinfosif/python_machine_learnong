7dab3b2146fb2559f0d50124c5cb73ad6d22b9c6
bioinfosif <ndaom403@gmail.com>
2022-02-11 18:24:55 +0000

    commit_msg

---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " commit message ?"
+read commit_msgs
+
+git add .
+git commit -m commit_msg
+git push https://github.com/bioinfosif/job_ML.git
+#!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
